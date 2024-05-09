764142b527f88cda64434d5843c0887376ce6d52
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-08 22:53:13 -0400

    more option for win

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod support;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let argumet = &args[1];
-    if argumet == "win" {
+    if argumet == "win" || argumet == "WIN" || argumet == "Win" || argumet == "windows" || argumet == "WINDOWS" || argumet == "Windows"{
         support::launch_vlc_win("bigbuckbunny_480.mp4");
     } else {
           support::launch_vlc_linux("/home/ssukijth/dev/hls/bigbuckbunny_480.mp4");
