8addd2f94b36a54bc021a71905ddce8f1f128e45
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-09 12:58:24 -0400

    Remove launch win and lin function, and use only one function for both

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,14 @@ fn main() {
     //check the argument
     // if it is equal to WIN, WINDOW, WINDOWS then call the function launch_vlc_win
     // if it is equal to LIN, LINUX then call the function launch_vlc_linux
+    let video_file;
+    let vlc_path;
     if argument == "WIN" || argument == "WINDOWS" || argument == "WINDOW"{
-        let video_file = "C:\\Users\\ssuki\\rust-demo\\bigbuckbunny_480.mp4";
-        support::launch_vlc_win(video_file);
+        video_file = "C:\\Users\\ssuki\\rust-demo\\bigbuckbunny_480.mp4";
+        vlc_path = "C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe";
     } else { //if the argument is not equal to WIN, WINDOW, WINDOWS then call the function launch_vlc_linux
-        let video_file = "/home/ssukijth/dev/hls/bigbuckbunny_480.mp4";
-          support::launch_vlc_linux(video_file);
-    }    
+        video_file = "/home/ssukijth/dev/hls/bigbuckbunny_480.mp4";
+        vlc_path = "/usr/bin/vlc";
+    }
+    support::launch_vlc(video_file, vlc_path)
 }
--- src/support.rs
+++ src/support.rs
@@ -1,37 +1,18 @@
 //function to launch vlc with the given file
-pub fn launch_vlc_linux(file: &str){
-     println!("Launching VLC with file: {}", file);
+pub fn launch_vlc(video_file: &str, vlc_path: &str) {
+     println!("Launching VLC with file: {}",video_file);
      //call bash command to launch vlc
-        std::process::Command::new("cvlc")
+        std::process::Command::new(vlc_path)
             .arg("--fullscreen")
             .arg("--play-and-exit") //flag to play and exit
             .arg("--no-video-title-show") //flag to remove video title
             .arg("--no-qt-error-dialogs") //flag to remove qt error dialogs
             .arg("--no-qt-name-in-title") //flag to remove qt name in title
-                .arg(file)
+                .arg(video_file)
             .spawn()
             .expect("VLC failed to start");
     //Note:
     // Up arrow key increase the volume
     // Down arrow key decrease the volume
     // Right arrow key forward the videogit 
-}
-
-//function to Launch vlc with a file in windows
-pub fn launch_vlc_win(file: &str) {
-    //use std::process to launch vlc with flag and a video file
-    let _output = std::process::Command::new("C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe") //path to vlc
-        .arg("--fullscreen") //flag to remove embedded video
-        .arg("--play-and-exit") //flag to play and exit
-        .arg("--no-video-title-show") //flag to remove video title
-        .arg("--no-qt-error-dialogs") //flag to remove qt error dialogs
-        .arg("--no-qt-name-in-title") //flag to remove qt name in title
-        .arg(file)
-        .output()
-        .expect("failed to execute process");
-    //Note:
-    // Up arrow key: increase the volume
-    // Down arrow key: decrease the volume
-    // Right arrow key: skip ahead by 10 seconds
-    // Left arrow key: skip back by 10 seconds
 }
