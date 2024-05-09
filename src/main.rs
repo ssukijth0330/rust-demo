78b56c9476e9184cd8ee6018b369f53536cc4f55
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-09 10:15:38 -0400

    use fullpath of video file

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     // if it is equal to WIN, WINDOW, WINDOWS then call the function launch_vlc_win
     // if it is equal to LIN, LINUX then call the function launch_vlc_linux
     if argument == "WIN" || argument == "WINDOWS" || argument == "WINDOW"{
-        let video_file = "bigbuckbunny_480.mp4";
+        let video_file = "C:\\Users\\ssuki\\rust-demo\\bigbuckbunny_480.mp4";
         support::launch_vlc_win(video_file);
     } else { //if the argument is not equal to WIN, WINDOW, WINDOWS then call the function launch_vlc_linux
         let video_file = "/home/ssukijth/dev/hls/bigbuckbunny_480.mp4";
